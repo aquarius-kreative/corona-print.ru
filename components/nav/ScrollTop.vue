d220229922b71b83700ba26bdd19fbdcd48ac569
alxhar <haritonov.aka@gmail.com>
2018-02-14 16:59:49 +0300

    dev update

---
+++ components/nav/ScrollTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-btn fab dark fixed bottom right small color="primary" @click="$vuetify.goTo('.cp-header-menu', {
+          duration: 800,
+          offset: 0,
+          easing: 'easeInOutCubic'
+        })">
+      <v-icon dark>keyboard_arrow_up</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'scroll-top'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,19 +4,22 @@
     <mobile-toolbar/>
     <nuxt/>
     <corona-footer/>
+    <scroll-top/>
   </v-app>
 </template>
 
 <script>
   import DesktopToolbar from '~/components/nav/DesktopToolbar'
   import MobileToolbar from '~/components/nav/MobileToolbar'
+  import ScrollTop from '~/components/nav/ScrollTop'
   import CoronaFooter from '~/components/footer/CoronaFooter'
 
   export default {
     components: {
       DesktopToolbar,
       MobileToolbar,
-      CoronaFooter
+      CoronaFooter,
+      ScrollTop
     }
   }
 </script>
